2dbc20712b8656faccb2b3b2b75236b9cfdcc6b3
BT_C09_04 <mobileappdev@bths.edu>
2026-05-21 09:03:18 -0400

    Initial Commit

---
+++ PogoParty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PogoParty
+//
+//  Created by Student on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PogoParty/PogoPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PogoPartyApp.swift
+//  PogoParty
+//
+//  Created by Student on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PogoPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c98af2f4ebfab990af847cdde45888c0588451b5
BT_C09_04 <mobileappdev@bths.edu>
2026-06-03 09:29:38 -0400

    Working on including all slimes on the screen; right now touch doesn't work

--- PogoParty/GameScene.swift
+++ PogoParty/GameScene.swift
@@ -11,17 +11,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     private var playerPosition = CGPoint(x: 150, y: 300)
-    var player = SKSpriteNode(imageNamed: "tile000")
+    var greenPlayer = SKSpriteNode(imageNamed: "tile000")
+    var bluePlayer = SKSpriteNode(imageNamed: "tile050")
+    var redPlayer = SKSpriteNode(imageNamed: "tile100")
+    var yellowPlayer = SKSpriteNode(imageNamed: "tile150")
     private var isJumping = false
     var moveSpeed: CGFloat = 400.0
-    let textureAtlas = SKTextureAtlas(named: "green_slime_sprites")
     
     override func didMove(to view: SKView) {
         setupScene()
-        setupPlayer(c: "green", tileNum: 0)
-//        setupPlayer(c: "blue", tileNum: 50)
-//        setupPlayer(c: "red", tileNum: 100)
-//        setupPlayer(c: "yellow", tileNum: 150)
+        
+        greenPlayer.name = "player"
+        bluePlayer.name = "player"
+        redPlayer.name = "player"
+        yellowPlayer.name = "player"
+        
+        setupPlayer(player: greenPlayer, c: "green", tileNum: 0)
+        setupPlayer(player: bluePlayer, c: "blue", tileNum: 50)
+        setupPlayer(player: redPlayer, c: "red", tileNum: 100)
+        setupPlayer(player: yellowPlayer, c: "yellow", tileNum: 150)
     }
     
     private func setupScene() {
@@ -29,14 +37,15 @@ class GameScene: SKScene {
         size = view!.bounds.size
     }
     
-    private func setupPlayer(c: String, tileNum: Int) {
-        player.position = CGPoint(x: 300 + player.size.width, y: player.size.height + 100)
+    private func setupPlayer(player: SKSpriteNode, c: String, tileNum: Int) {
         player.setScale(4)
-        staticAnimation(tileNum: tileNum)
+        player.position = CGPoint(x: 300 + player.size.width, y: player.size.height + 100)
+        player.zPosition = 5
+        staticAnimation(player: player, tileNum: tileNum)
         addChild(player)
     }
     
-    private func staticAnimation(tileNum: Int){
+    private func staticAnimation(player: SKSpriteNode, tileNum: Int){
         player.removeAction(forKey: "textureLoop")
         
         var staticAnimation = [SKTexture]()
@@ -52,7 +61,7 @@ class GameScene: SKScene {
             default:
                 name = "tile\(frameNum)"
             }
-            staticAnimation.append(textureAtlas.textureNamed(name))
+            staticAnimation.append(SKTexture(imageNamed: name))
         }
         
         let animation = SKAction.animate(with: staticAnimation, timePerFrame: 0.12)
@@ -60,7 +69,7 @@ class GameScene: SKScene {
         player.run(repeatForever, withKey: "textureLoop")
     }
     
-    private func otherAnimation(tileNum: Int) {
+    private func otherAnimation(player: SKSpriteNode, tileNum: Int) {
         player.removeAction(forKey: "textureLoop")
         var playerAnimation = [SKTexture]()
         
@@ -74,7 +83,7 @@ class GameScene: SKScene {
             default:
                 name = "tile\(frameNum)"
             }
-            playerAnimation.append(textureAtlas.textureNamed(name))
+            playerAnimation.append(SKTexture(imageNamed: name))
         }
         let animation = SKAction.animate(with: playerAnimation, timePerFrame: 0.15)
         let repeatForever = SKAction.repeatForever(animation)
@@ -82,23 +91,60 @@ class GameScene: SKScene {
     }
    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let location = touch.location(in: self)
-            let distance = hypot(player.position.x - location.x, player.position.y - location.y)
-            let duration = distance / moveSpeed
-            
-            isJumping = true
-            otherAnimation(tileNum: 20)
-            move(to: location, time: duration)
+        guard let touch = touches.first else {
+            return
+        }
+
+        if let playerNode = self.childNode(withName: "player") as? SKSpriteNode {
+            self.handleTouch(player: playerNode, touch: touch)
+        } else if let fallbackPlayer = self.children.first(where: { $0 is SKSpriteNode }) as? SKSpriteNode {
+            self.handleTouch(player: fallbackPlayer, touch: touch)
+        } else {
+            print("No SKSpriteNodes in this scene yet.")
         }
     }
     
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        isJumping = false
-//        staticAnimation(tileNum: 0)
-//    }
+    func handleTouch(player: SKSpriteNode, touch: UITouch) {
+        let location = touch.location(in: self)
+        let touchedNode = self.atPoint(location)
+        
+        if let playerNode = touchedNode as? SKSpriteNode, playerNode.name == "player" {
+            self.handleTouch(player: playerNode, touch: touch)
+        } else if let fallbackPlayer = self.children.first(where: { $0.name == "player" }) as? SKSpriteNode {
+            // Fallback if they tap the empty white background
+            self.handleTouch(player: fallbackPlayer, touch: touch)
+        }
+        
+        let distance = hypot(player.position.x - location.x, player.position.y - location.y)
+        let duration = TimeInterval(distance / moveSpeed)
+        
+//        isJumping = true
+//        otherAnimation(player: player, tileNum: 20)
+//        move(player: player, to: location, time: duration)
+        
+        let baseTileNum = determineBaseTileNum(for: player)
+        otherAnimation(player: player, tileNum: baseTileNum + 20)
+        
+        move(player: player, to: location, time: duration)
+    }
     
-    private func move(to location: CGPoint, time: CGFloat) {
+    private func determineBaseTileNum(for player: SKSpriteNode) -> Int {
+        guard let identity = player.userData?.value(forKey: "colorID") as? String else {
+            return 0
+        }
+        
+        switch identity {
+        case "blue":   return 50
+        case "red":    return 100
+        case "yellow": return 150
+        default:       return 0
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    private func move(player: SKSpriteNode, to location: CGPoint, time: CGFloat) {
         player.removeAction(forKey: "movingAction")
         
         let moveAction = SKAction.move(to: location, duration: time)
@@ -106,7 +152,8 @@ class GameScene: SKScene {
         let doneMoving = SKAction.run { [weak self] in
             guard let self = self else { return }
             self.isJumping = false
-            self.staticAnimation(tileNum: 0)
+            let baseTileNum = self.determineBaseTileNum(for: player)
+            self.staticAnimation(player: player, tileNum: 0)
         }
         
         let sequence = SKAction.sequence([moveAction, doneMoving])
